7ed3a6520f046663cd4effb8174a8feff287cf50
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2022-04-07 21:05:03 -0300

    Use rem_eclid

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,78 +1,33 @@
 use core::fmt;
 
+const MINUTES_PER_DAY: i32 = 1440;
+const MINUTES_PER_HOUR: i32 = 60;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Clock {
-    total_minutes: i32,
+    minutes: i32,
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let hours_converted = (self.total_minutes / 60) % 24;
-        let minutes_converted = self.total_minutes % 60;
-        let hours_print = if hours_converted < 10 {
-            format!("0{}", hours_converted)
-        } else {
-            format!("{}", hours_converted)
-        };
-        let minutes_print = if minutes_converted < 10 {
-            format!("0{}", minutes_converted)
-        } else {
-            format!("{}", minutes_converted)
-        };
-        write!(f, "{}:{}", hours_print, minutes_print)
+        let hours_converted = self.minutes / MINUTES_PER_HOUR;
+        let minutes_converted = self.minutes % MINUTES_PER_HOUR;
+        write!(f, "{:02}:{:02}", hours_converted, minutes_converted)
     }
 }
 
 impl Clock {
-    fn convert_hours_to_minutes(hours: i32) -> i32 {
-        // ensure it will not overflow the max hours and convert to minutes
-        if hours < 0 {
-            let mut helper_hours = hours;
-            while helper_hours < 0 {
-                helper_hours += 24;
-            }
-            ((helper_hours + 24) % 24) * 60
-        } else {
-            (hours % 24) * 60
-        }
-    }
-
-    fn minutes_converted(minutes: i32) -> i32 {
-        // ensure it will not overflow the max minutes
-        if minutes < 0 {
-            let mut helper_minutes = minutes;
-            while helper_minutes < 0 {
-                helper_minutes += 1440;
-            }
-            helper_minutes
-        } else if minutes > 1440 {
-            let mut helper_minutes = minutes;
-            while helper_minutes > 1440 {
-                helper_minutes -= 1440;
-            }
-            helper_minutes
-        } else {
-            minutes
-        }
-    }
-    fn convert_to_minutes(hours: i32, minutes: i32) -> i32 {
-        let hours_in_minutes = Clock::convert_hours_to_minutes(hours);
-        let minutes_converted = Clock::minutes_converted(minutes);
-        let total = hours_in_minutes + minutes_converted;
-        let total = Clock::minutes_converted(total);
-        return total;
-    }
-
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let total_minutes = Clock::convert_to_minutes(hours, minutes);
-        Clock { total_minutes }
+        let hours_in_minutes = hours * 60;
+        let minutes = (hours_in_minutes + minutes).rem_euclid(MINUTES_PER_DAY);
+        Clock { minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let total_minutes = self.total_minutes + minutes;
-        let minutes_converted = Clock::minutes_converted(total_minutes);
+        let total_minutes = self.minutes + minutes;
+        let minutes_converted = (total_minutes).rem_euclid(MINUTES_PER_DAY);
         Clock {
-            total_minutes: minutes_converted,
+            minutes: minutes_converted,
         }
     }
 }
